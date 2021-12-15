229139693848f579dfaa9b51658fbfc041e5b6fa
Ricardo Quintas <rqcenter-web@yahoo.com>
2021-12-15 13:55:39 +0000

    sorting

--- js/index.js
+++ js/index.js
@@ -3,6 +3,10 @@ var parsed_csv;
 var start_time, end_time;
 var _aData = [];
 var _notInitializedYet = true;
+var _oSort = {
+    id: null,
+    sortAscending: null
+}
 
 // document.ready
 $(function () {
@@ -35,7 +39,8 @@ $(function () {
                     // Save result in a globally accessible var
                     parsed_csv = results;
                     _aData = results.data;
-                    buildTable();
+                    fixData();
+                    buildTable(null, false);
                     end_time = performance.now();
                 }
             });
@@ -52,6 +57,46 @@ $(function () {
     });
 
 });
+
+function fixData() {
+    var tempArr = [];
+    for (let i = 0; i < _aData.length; i++) {
+        var obj = _aData[i];
+        var arr;
+
+        if (obj["SOUND RECORDING TITLE"]) {
+            if (typeof obj["SOUND RECORDING TITLE"] !== "string") {
+                obj["SOUND RECORDING TITLE"] = obj["SOUND RECORDING TITLE"].toString()
+            }
+            arr = obj["SOUND RECORDING TITLE"].split("-");
+
+            if (arr[0]) {
+                obj["SOUND RECORDING TITLE"] = arr[0];
+            }
+
+            if (arr[1] && obj["ALBUM TITLE"] === "") {
+                obj["ALBUM TITLE"] = arr[1];
+            }
+
+            if (typeof obj["SOUND RECORDING TITLE"] !== "string") {
+                obj["SOUND RECORDING TITLE"] = obj["SOUND RECORDING TITLE"].toString()
+            }
+            if (typeof obj["FEATURED ARTIST"] !== "string") {
+                obj["FEATURED ARTIST"] = obj["FEATURED ARTIST"].toString()
+            }
+            if (typeof obj["ALBUM TITLE"] !== "string") {
+                obj["ALBUM TITLE"] = obj["ALBUM TITLE"].toString()
+            }
+        }
+
+        if (obj["FEATURED ARTIST"] !== undefined) {
+            tempArr.push(obj);
+        }
+    }
+
+    _aData = tempArr;
+
+};
 
 function prepareTheReportsAndNavigateNextPage() {
     $.LoadingOverlay("show");
@@ -135,7 +180,90 @@ function reportListeners() {
     return arr;
 }
 
-function buildTable(aFilteredItems) {
+function sortThis(oColumn) {
+    var oCurrentSortArrow = $('i', oColumn);
+
+    // remove previous sort assigned column
+    if (_oSort.id) {
+        $("#" + _oSort.id).removeClass("zrq-sorted");
+        $('i', "#" + _oSort.id).remove();
+    }
+
+    _oSort.id = oColumn.id;
+    _oSort.sortAscending = true;
+    $(oColumn).addClass("zrq-sorted");
+
+    if (!oCurrentSortArrow) {
+        $(oColumn).append("<i class='bi bi-arrow-up'></i>");
+    } else {
+        if (oCurrentSortArrow.hasClass("bi-arrow-up")) {
+            $('i', oColumn).remove();
+            _oSort.sortAscending = true;
+            $(oColumn).append("<i class='bi bi-arrow-down'></i>");
+        } else {
+            $('i', oColumn).remove();
+            _oSort.sortAscending = false;
+            $(oColumn).append("<i class='bi bi-arrow-up'></i>");
+        }
+    }
+
+    sortTable();
+}
+
+function sortTable() {
+
+    var sColumn = "";
+    var sortTypeIsString = false;
+
+    $("#myTable tbody").empty();
+
+    switch (_oSort.id) {
+        case "sortArtist":
+            sColumn = "FEATURED ARTIST";
+            sortTypeIsString = true;
+            break;
+        case "sortSong":
+            sColumn = "SOUND RECORDING TITLE";
+            sortTypeIsString = true;
+            break;
+        case "sortAlbum":
+            sColumn = "ALBUM TITLE";
+            sortTypeIsString = true;
+            break;
+        case "sortPerformance":
+            sColumn = "ACTUAL TOTAL PERFORMANCES";
+            sortTypeIsString = false;
+            break;
+        case "sortPlayFreq":
+            sColumn = "PLAY FREQUENCY";
+            sortTypeIsString = false;
+            break;
+    }
+
+    if (_oSort.sortAscending) {
+        if (sortTypeIsString) {
+            _aData = _aData.sort((a, b) => {
+
+                return b[sColumn].localeCompare(a[sColumn], 'en', { sensitivity: 'base' });
+            });
+        } else {
+            _aData.sort((a, b) => (a[sColumn] < b[sColumn] ? 1 : -1))
+        }
+    } else {
+        if (sortTypeIsString) {
+            _aData = _aData.sort((a, b) => {
+
+                return a[sColumn].localeCompare(b[sColumn], 'en', { sensitivity: 'base' });
+            });
+        } else {
+            _aData.sort((a, b) => (a[sColumn] > b[sColumn] ? 1 : -1))
+        }
+    }
+
+    buildTable(_aData, true);
+}
+
+function buildTable(aFilteredItems, bJustRenderBody) {
     $.LoadingOverlay("show");
     var aItems = []
     if (aFilteredItems) {
@@ -146,30 +274,31 @@ function buildTable(aFilteredItems) {
 
     if (aItems) {
         try {
-            var oTable = $("#myTable");
-            var str = "<table class='table table-striped'>";
-            str += "<thead>";
-            str += "<tr>";
-            str += "<th scope='col'>Artist</th>";
-            str += "<th scope='col'>Song</th>";
-            str += "<th scope='col'>Album</th>";
-            str += "<th scope='col'>Performances</th>";
-            str += "<th scope='col'>Play freq.</th>";
-            str += "</tr>";
-            str += "</thead>";
-            str += "<tbody>";
+            var oTable;
+
+            if (bJustRenderBody) {
+                oTable = $("#zrqTbody");
+            } else {
+                oTable = $("#myTable");
+                var str = "<table id='zrqTable' class='table table-striped'>";
+                str += "<thead>";
+                str += "<tr>";
+                str += "<th id='sortArtist' onClick='sortThis(this)'scope='col'>Artist</th>";
+                str += "<th id='sortSong' onClick='sortThis(this)' scope='col'>Song</th>";
+                str += "<th id='sortAlbum' onClick='sortThis(this)' scope='col'>Album</th>";
+                str += "<th id='sortPerformance' onClick='sortThis(this)' scope='col'>Perf.</th>";
+                str += "<th id='sortPlayFreq' onClick='sortThis(this)' scope='col'>Play freq.</th>";
+                str += "</tr>";
+                str += "</thead>";
+                str += "<tbody id='zrqTbody'>";
+            }
+
+
             for (let i = 0; i < aItems.length; i++) {
                 var obj = aItems[i];
                 var arr = [];
                 if (obj["FEATURED ARTIST"] === "" || obj["SOUND RECORDING TITLE"] === "") {
                     continue;
-                }
-
-                if (obj["SOUND RECORDING TITLE"]) {
-                    if (typeof obj["SOUND RECORDING TITLE"] !== "string") {
-                        obj["SOUND RECORDING TITLE"] = obj["SOUND RECORDING TITLE"].toString()
-                    }
-                    arr = obj["SOUND RECORDING TITLE"].split("-");
                 }
 
                 str += "<tr>";
@@ -179,11 +308,11 @@ function buildTable(aFilteredItems) {
                 str += "</td>";
 
                 str += "<td>";
-                str += arr[0] ? arr[0] : "";
+                str += obj["SOUND RECORDING TITLE"];
                 str += "</td>";
 
                 str += "<td>";
-                str += arr[1] ? arr[1] : "";
+                str += obj["ALBUM TITLE"];
                 str += "</td>";
 
                 str += "<td>";
@@ -196,8 +325,11 @@ function buildTable(aFilteredItems) {
 
                 str += "</tr>";
             }
-            str += "</tbody>";
-            str += "</table>";
+
+            if (!bJustRenderBody) {
+                str += "</tbody>";
+                str += "</table>";
+            }
 
             oTable.html(str);
 
@@ -228,7 +360,7 @@ function clearFilter() {
     $("#zrq-search").val("");
     $("#myTable thead").remove();
     $("#myTable tbody").remove();
-    buildTable();
+    buildTable(null, false);
 }
 
 function onCloseModalClick() {
@@ -256,7 +388,7 @@ function initializeEvents() {
                 $("#zrq-search").val("");
                 $("#myTable thead").remove();
                 $("#myTable tbody").remove();
-                buildTable();
+                buildTable(null, false);
             } else {
                 if (sValue.length >= 2) {
                     if ($('#rbArtist').is(':checked')) {
@@ -291,24 +423,15 @@ function filterTable(sValue, sColumnToFilter) {
                 case "song":
                     str = n["SOUND RECORDING TITLE"];
                     str = str ? str : "";
-                    str = str.toString();
-                    var arr = str.split("-");
-                    if (arr[0]) {
-                        str = arr[0];
-                    }
-                    break;
-                case "album":
-                    str = n["SOUND RECORDING TITLE"];
                     str = str ? str : "";
                     str = str.toString();
-                    var arr = str.split("-");
-                    if (arr[1]) {
-                        str = arr[1];
-                    }
+                    break;
+                case "album":
+                    str = n["ALBUM TITLE"];
+                    str = str ? str : "";
+                    str = str.toString();
                     break;
                 default:  // search ALL columns
-                    var arrAll;
-
                     var str1 = n["FEATURED ARTIST"];
                     str1 = str1 ? str1 : "";
                     str1 = str1.toString();
@@ -316,18 +439,11 @@ function filterTable(sValue, sColumnToFilter) {
                     var str2 = n["SOUND RECORDING TITLE"];
                     str2 = str2 ? str2 : "";
                     str2 = str2.toString();
-                    arrAll = str2.split("-");
-                    if (arrAll[0]) {
-                        str2 = arrAll[0];
-                    }
 
-                    var str3 = n["SOUND RECORDING TITLE"];
+                    var str3 = n["ALBUM TITLE"];
                     str3 = str3 ? str3 : "";
                     str3 = str3.toString();
-                    arrAll = str3.split("-");
-                    if (arrAll[1]) {
-                        str3 = arrAll[1];
-                    }
+
                     str = str1 + " " + str2 + " " + str3;
                     break;
             }
@@ -336,7 +452,7 @@ function filterTable(sValue, sColumnToFilter) {
         });
 
     $("#myTable").html("");
-    buildTable(arr);
+    buildTable(arr, false);
 }
 
 
